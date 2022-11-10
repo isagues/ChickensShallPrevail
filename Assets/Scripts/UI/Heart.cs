b389febe9da4f10692eb44e2a597a146f2c0edd0
Ignacio Sagues <isagues@itba.edu.ar>
2022-11-10 11:43:03 -0300

    Animacion del cocoro

---
+++ Assets/Scripts/UI/Heart.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class Heart : MonoBehaviour
+    {
+        [SerializeField] private List<Sprite> _hearts;
+        private Image _heart;
+        
+        [SerializeField] private float _speed = 10;
+        private float _timer = 1;
+        private int _index = 0;
+        
+        private void Start()
+        {
+            _heart = GetComponent<Image>();
+            UpdateHeartSprite();
+        }
+
+        private void Update()
+        {
+            _timer -= Time.deltaTime * _speed;
+
+            if (!(_timer <= 0)) return;
+
+            _index = (_index + 1) % _hearts.Count;
+            UpdateHeartSprite();
+            _timer = 1;
+
+        }
+        
+        private void UpdateHeartSprite() => _heart.sprite = _hearts[_index];
+    }
+}
